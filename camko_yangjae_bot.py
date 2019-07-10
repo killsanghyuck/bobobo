72e1893f818923cc749c9623a1228d0e936f1c11
길상혁 <kill5038@gmail.com>
2019-07-10 14:23:27 +0900

    캠코양재 주소 변경

--- camko_yangjae_bot.py
+++ camko_yangjae_bot.py
@@ -11,7 +11,7 @@ from importlib import reload
 
 
 #봇 기본 정보
-PARK_HOST_URL = 'http://121.134.88.17:8090'
+PARK_HOST_URL = 'http://14.52.108.156:8090/'
 LOGIN_URL = '/account/login.asp'
 SEARCH_CAR_NUMBER_URL = '/discount/discount_regist.asp'
 ADD_ACTION_URL = '/discount/discount_regist.asp'
