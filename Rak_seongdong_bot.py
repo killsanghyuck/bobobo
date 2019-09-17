e2ff4ccb4b40b1fe6d2e70434b0c2bae3522db19
kill5038 <blain1@naver.com>
2019-09-17 19:41:39 +0900

    락성동 야간권 추가

--- Rak_seongdong_bot.py
+++ Rak_seongdong_bot.py
@@ -27,7 +27,13 @@ class RakseongdongBot(BotInterface):
     def __init__(self, reservation):
         self.k_car_num = reservation['k_car_num']
         self.entry_date = reservation['entry_date']
-        self.discount_id = 99
+        self.ticket_state = reservation['ticket_state']
+
+        if self.ticket_state == 0:
+            self.discount_id = 99
+        else:
+            self.discount_id = 94
+
         self.s = requests.Session()
 
     def login(self):
@@ -55,13 +61,24 @@ class RakseongdongBot(BotInterface):
                     else:
                         car_num = tr_list[i].select('td')[1].text.split('[')[0].strip()
                         parking_time = tr_list[i].select('td')[3].text.strip()
+                        entry_time = tr_list[i].select('td')[2].text.strip()
+                        self.entry_time = datetime.datetime.strptime(entry_time, "%Y-%m-%d %H:%M:%S").strftime("%H")                                                                    
                         if car_num == self.k_car_num and len(parking_time) == 5:
                             self.chk = tr_list[i].select('td')[0].select('input')[0]['value']
                             flag = True
                         elif car_num == self.k_car_num and parking_time > '20':
                             self.chk = tr_list[i].select('td')[0].select('input')[0]['value']
-                            flag = True
+                            flag = True        
+    
+                        if self.check_time(): flag = False
         return flag
+
+    def check_time(self):
+      flag = False
+      entry_time = int(self.entry_time)
+      if (entry_time < 18 and entry_time >= 7) and self.ticket_state == 1:
+        print(u'이용가능시간 아님(이거 아래 입차확인 불가로 표시함)')
+        flag = True        
 
     def process(self):
         flag = False
