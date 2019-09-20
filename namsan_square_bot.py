754645fe20e7c7c9c9b66cd68979bfc4b1685e22
killsanghyuck <blain1@naver.com>
2019-09-20 17:01:11 +0900

    namsan edit

--- namsan_square_bot.py
+++ namsan_square_bot.py
@@ -62,6 +62,8 @@ class namsansquareBot(BotInterface):
                     else:
                         car_num = tr_list[i].select('td')[1].text.split('[')[0].strip()
                         parking_time = tr_list[i].select('td')[3].text.strip()
+                        entry_time = tr_list[i].select('td')[2].text.strip()
+                        self.entry_time = datetime.datetime.strptime(entry_time, "%Y-%m-%d %H:%M:%S").strftime("%H")
                         if car_num == self.k_car_num and len(parking_time) == 5:
                             self.chk = tr_list[i].select('td')[0].select('input')[0]['value']
                             flag = True
