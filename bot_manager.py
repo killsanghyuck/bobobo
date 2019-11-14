ec911e74bac728aba29b4e8b3345468fe97a47f3
길상혁 <kill5038@gmail.com>
2019-11-14 11:28:33 +0900

    어바니엘 염창 봇 추가

--- bot_manager.py
+++ bot_manager.py
@@ -100,7 +100,7 @@ from gamjung_bot import GamjungBot
 from js_hotel_bot import JsHotelBot
 from pacific_bot import PacificBot
 from Hankyung_bot import hankyungBot
-from foreheal_bot import forehealBot
+from urbanil_yeom_bot import urbanilyeomBot
 
 #넥스파
 from gran_seoul import GranSeoulBot
