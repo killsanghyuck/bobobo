f15e27529847ecc402c628f3a6f9f5d2a3331327
길상혁 <kill5038@gmail.com>
2019-11-26 17:31:50 +0900

    L7홍대 봇 수정

--- l7_hong_bot.py
+++ l7_hong_bot.py
@@ -11,7 +11,7 @@ from importlib import reload
 
 
 #봇 기본 정보
-PARK_HOST_URL = 'http://61.73.127.248:8090'
+PARK_HOST_URL = 'http://121.162.237.142:8090'
 LOGIN_URL = '/account/login.asp'
 SEARCH_CAR_NUMBER_URL = '/discount/discount_regist.asp'
 ADD_ACTION_URL = '/discount/discount_regist.asp'
