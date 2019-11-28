2c253329eb41e5df51dbda651dad86bc69e0dfd7
길상혁 <kill5038@gmail.com>
2019-11-28 19:22:35 +0900

    코스모타워 주소 변경

--- cosmo_tower_bot.py
+++ cosmo_tower_bot.py
@@ -12,7 +12,7 @@ from importlib import reload
 
 
 #봇 기본 정보
-PARK_HOST_URL = 'http://175.193.76.127:8090'
+PARK_HOST_URL = 'http://14.40.73.170:8090'
 LOGIN_URL = '/account/login.asp'
 SEARCH_CAR_NUMBER_URL = '/discount/discount_regist.asp'
 ADD_ACTION_URL = '/discount/discount_regist.asp'
