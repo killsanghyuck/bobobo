8ebcc7508c449fcdc7c6224848cc1c2d148c5b32
길상혁 <kill5038@gmail.com>
2019-12-06 14:59:57 +0900

    센터플레이스 주소 수정

--- center_place_bot.py
+++ center_place_bot.py
@@ -11,7 +11,7 @@ from importlib import reload
 
 
 #봇 기본 정보
-PARK_HOST_URL = 'http://114.205.71.136:8090'
+PARK_HOST_URL = 'http://114.205.71.214:8090'
 LOGIN_URL = '/account/login.asp'
 SEARCH_CAR_NUMBER_URL = '/discount/discount_regist.asp'
 ADD_ACTION_URL = '/discount/discount_regist.asp'
