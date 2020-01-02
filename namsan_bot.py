0941d297b857dade489374371c44dd8888465a38
길상혁 <kill5038@gmail.com>
2020-01-02 13:42:36 +0900

    우리금융남산 수정

--- namsan_bot.py
+++ namsan_bot.py
@@ -12,7 +12,7 @@ from importlib import reload
 
 
 #봇 기본 정보
-PARK_HOST_URL = 'http://1.215.35.91:8090'
+PARK_HOST_URL = 'http://1.215.35.93:8090'
 LOGIN_URL = '/account/login.asp'
 SEARCH_CAR_NUMBER_URL = '/discount/discount_regist.asp'
 ADD_ACTION_URL = '/discount/discount_regist.asp'
