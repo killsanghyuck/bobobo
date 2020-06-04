e86da9a7bf7d14ff3defe29026c9d46ecf21aba4
길상혁 <kill5038@gmail.com>
2020-06-04 23:00:05 +0900

    점프밀라노 수정

--- jump_bot.py
+++ jump_bot.py
@@ -12,7 +12,7 @@ from importlib import reload
 
 
 #봇 기본 정보
-PARK_HOST_URL = 'http://218.232.123.157:8090'
+PARK_HOST_URL = 'http://218.232.123.182:8090'
 LOGIN_URL = '/account/login.asp'
 SEARCH_CAR_NUMBER_URL = '/discount/discount_regist.asp'
 ADD_ACTION_URL = '/discount/discount_regist.asp'
