022479e8222874da06447b8f3f132465c62aa31a
길상혁 <kill5038@gmail.com>
2020-07-16 13:08:36 +0900

    포도몰 업데이트

--- podo_mall_bot.py
+++ podo_mall_bot.py
@@ -6,6 +6,7 @@ import sys
 import time
 import datetime
 import json
+from urllib import parse
 
 from bot_interface import BotInterface
 
@@ -19,17 +20,16 @@ SEARCH_CAR_NUMBER_URL = '/discount/registration/listForDiscount'
 ADD_ACTION_URL = '/discount/registration/save'
 LIST_FIND = '/discount/state/list/doListMst'
 LOGIN_INFO = {
+    'referer': '',
     'userId': 'kakaot',
     'userPwd': '123456'
 }
 AREA_ID = '433'
 
-CORP_NAME = '카카오T'
-
 class PodoMallBot(BotInterface):
     def __init__(self, reservation):
         self.k_car_num = reservation['k_car_num']
-        self.entry_date = reservation['entry_date']
+        self.entry_date = reservation['entry_date'].replace("-","")
         self.duration = reservation['duration']
         self.discount_id = 11
         self.s = requests.Session()
@@ -44,15 +44,18 @@ class PodoMallBot(BotInterface):
 
     def find_car_number(self):
         flag = False
-        find_req = self.s.post(PARK_HOST_URL + SEARCH_CAR_NUMBER_URL, data={'carNo': self.k_car_num, 'entryDate': self.entry_date})
+        find_req = self.s.post(PARK_HOST_URL + SEARCH_CAR_NUMBER_URL + "?carNo=" + parse.quote(self.k_car_num) + '&entryDate=' + self.entry_date)
         data = json.loads(find_req.content)
         self.returned_car_no = ''
+        self.entry_time = '19'
         if len(data) >= 1:
-            self.id = data[0]['id']
-            self.i_lot_area = data[0]['iLotArea']
-            self.returned_car_no = data[0]['carNo']
-
-        if self.returned_car_no == self.k_car_num: flag = True
+            for car in data:
+                if self.k_car_num in car['carNo']:
+                    self.id = car['id']
+                    self.i_lot_area = car['iLotArea']
+                    self.returned_car_no = car['carNo']
+                    self.entry_time = datetime.datetime.strptime(car['entryDateToString'], "%Y-%m-%d %H:%M:%S").strftime("%H")
+                    flag = True
         return flag
 
     def process(self):
@@ -60,10 +63,8 @@ class PodoMallBot(BotInterface):
         def add_action(self):
             ADD_ACTION_PARAMS = {
                 'peId': self.id,
-                'corp': CORP_NAME,
                 'discountType': self.discount_id,
                 'carno': self.k_car_num,
-                'iLotArea': self.i_lot_area,
                 'memo': ''
             }
             add_req = self.s.post(PARK_HOST_URL + ADD_ACTION_URL, data=ADD_ACTION_PARAMS)
@@ -71,19 +72,17 @@ class PodoMallBot(BotInterface):
 
         def list_find(k_car_num):
             flag = True
-        #     LIST_FIND_PARAMS = {
-        #         'startDate': self.entry_date,
-        #         'endDate': self.entry_date,
-        #         'account_no': 'kakaot',
-        #         'carno': self.k_car_num
-        #     }
-        #     list_req = self.s.post(PARK_HOST_URL + LIST_FIND, data=LIST_FIND_PARAMS)
-        #     results = json.loads(list_req.content)
-        #     results = results['data']
-        #     if results[0]['carno'] == self.k_car_num: flag = True
-        #     return flag
-        #
-        # if add_action(self) and list_find(self): flag = True
+            # LIST_FIND_PARAMS = {
+            #     'startDate': self.entry_date,
+            #     'endDate': self.entry_date,
+            #     'account_no': 'parkhere81',
+            #     'carno': self.k_car_num,
+            #     'iLotArea': 81
+            # }
+            # list_req = self.s.post(PARK_HOST_URL + LIST_FIND, data=LIST_FIND_PARAMS)
+            # results = json.loads(list_req.content)
+            # results = results['data']
+            # if results[0]['carno'] == self.k_car_num: flag = True
             return flag
 
         if add_action(self) and list_find(self): flag = True
