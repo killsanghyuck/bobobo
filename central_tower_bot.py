b9b2353d936dff7cfc70a3b717da1e0692eb60c5
길상혁 <kill5038@gmail.com>
2021-01-25 17:41:37 +0900

    센트럴타워 주소변경

--- central_tower_bot.py
+++ central_tower_bot.py
@@ -11,7 +11,7 @@ from importlib import reload
 
 
 #봇 기본 정보
-PARK_HOST_URL = 'http://221.147.57.61:8090'
+PARK_HOST_URL = 'http://183.96.157.89:8090'
 LOGIN_URL = '/account/login.asp'
 SEARCH_CAR_NUMBER_URL = '/discount/discount_regist.asp'
 ADD_ACTION_URL = '/discount/discount_regist.asp'
